223c10354444d571bbe681b88deb3f05c89c6b2b
Dika-Doki <dikadoki3@gmail.com>
2026-06-26 00:09:56 +0700

    update website

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -1,9 +1,9 @@
-import gallery1 from "../assets/gallery-1.jpg";
-import gallery2 from "../assets/gallery-2.jpg";
-import gallery3 from "../assets/gallery-3.jpg";
-import gallery4 from "../assets/gallery-4.jpg";
-import gallery5 from "../assets/gallery-5.jpg";
-import gallery6 from "../assets/gallery-6.jpg";
+import gallery1 from "../assets/gallery-1.jpeg";
+import gallery2 from "../assets/gallery-2.jpeg";
+import gallery3 from "../assets/gallery-3.jpeg";
+import gallery4 from "../assets/gallery-4.jpeg";
+import gallery5 from "../assets/gallery-5.jpeg";
+import gallery6 from "../assets/gallery-6.jpeg";
 
 export const galleryImages = [
   {
