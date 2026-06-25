6711d301febd133afc35b6fbcecdf922d4c13c64
Dika-Doki <dikadoki3@gmail.com>
2026-06-26 00:50:05 +0700

    update website

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -1,18 +1,18 @@
-import hero1 from "../assets/gallery1.jpg";
-import hero2 from "../assets/gallery10.jpg";
-import hero3 from "../assets/gallery13.jpg";
+import gallery1 from "../assets/gallery1.jpg";
+import gallery10 from "../assets/gallery10.jpg";
+import gallery13 from "../assets/gallery13.jpg";
 
 export const heroSlides = [
   {
-    image: hero1,
+    image: gallery1,
     label: "Wedding",
   },
   {
-    image: hero2,
+    image: gallery10,
     label: "Portrait Session",
   },
   {
-    image: hero3,
+    image: gallery13,
     label: "Brand",
   },
 ];
